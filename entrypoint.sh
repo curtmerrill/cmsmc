d4c335f32a3428f246999416b773dd91067ad7e8
Curt Merrill <curt@curtmerrill.com>
2024-09-10 14:29:03 -0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-gunicorn --workers 3 --bind 0.0.0.0:8088 cmsmc.wsgi
+granian --interface wsgi --port 8088 cmsmc.wsgi:application
